f8cd0ac435836e858489edf9e965b17852fbef6e
Yasir Ali <ali.yasir0@gmail.com>
2016-09-20 17:42:54 +0500

    add Base ViewModel that will be inherited by all folowwing ViewModel classes

---
+++ Sociocon/Sociocon/Sociocon/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sociocon.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BaseViewModel () { }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}

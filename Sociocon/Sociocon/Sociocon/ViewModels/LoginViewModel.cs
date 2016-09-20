1bab45ffed411e59721e29c97b2448fca5eee206
Yasir Ali <ali.yasir0@gmail.com>
2016-09-20 17:49:06 +0500

    add login ViewModel with properties of username and password

---
+++ Sociocon/Sociocon/Sociocon/ViewModels/LoginViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sociocon.ViewModels
+{
+    class LoginViewModel : BaseViewModel
+    {
+        string username;
+        string password;
+
+        public LoginViewModel() { }
+
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                if ( value == username )
+                    return;
+
+                username = value;
+                OnPropertyChanged("username");
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if ( value == password )
+                    return;
+
+                password = value;
+                OnPropertyChanged("password");
+            }
+        }
+    }
+}
